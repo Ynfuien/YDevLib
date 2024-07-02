f0f08db724ac98c0ebd435b1812d6d63c1789c78
Ynfuien <ynfuien@gmail.com>
2024-07-02 23:23:32 +0200

    Added date-time and duration to common placeholders

--- src/main/java/pl/ynfuien/ydevlib/utils/CommonPlaceholders.java
+++ src/main/java/pl/ynfuien/ydevlib/utils/CommonPlaceholders.java
@@ -3,6 +3,9 @@ package pl.ynfuien.ydevlib.utils;
 import org.bukkit.Location;
 import org.bukkit.World;
 
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 
 public class CommonPlaceholders {
@@ -22,5 +25,29 @@ public class CommonPlaceholders {
         placeholders.put(pfx+"pitch", df.format(loc.getPitch()));
         World world = loc.getWorld();
         placeholders.put(pfx+"world", world != null ? world.getName() : "undefined");
+    }
+
+    public static void setDateTime(HashMap<String, Object> placeholders, long timestamp, String prefix) {
+        String pfx = prefix == null ? "" : prefix + "-";
+
+        LocalDateTime date = new Timestamp(timestamp).toLocalDateTime();
+        // Date
+        placeholders.put(pfx+"year", date.getYear());
+        placeholders.put(pfx+"month", String.format("%02d", date.getMonthValue()));
+        placeholders.put(pfx+"day", String.format("%02d", date.getDayOfMonth()));
+        // Time
+        placeholders.put(pfx+"hour", String.format("%02d", date.getHour()));
+        placeholders.put(pfx+"minute", String.format("%02d", date.getMinute()));
+        placeholders.put(pfx+"second", String.format("%02d", date.getSecond()));
+    }
+
+    public static void setDuration(HashMap<String, Object> placeholders, long time, String prefix) {
+        String pfx = prefix == null ? "" : prefix + "-";
+
+        Duration duration = Duration.ofMillis(time);
+        placeholders.put(pfx+"days", duration.toDaysPart());
+        placeholders.put(pfx+"hours", duration.toHoursPart());
+        placeholders.put(pfx+"minutes", duration.toMinutesPart());
+        placeholders.put(pfx+"seconds", duration.toSecondsPart());
     }
 }
